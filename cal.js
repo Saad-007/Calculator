2ec8ff7a1ce443840251864cfdba458b4054a9bd
Saad-007 <saadsafeer223@gmail.com>
2025-04-06 13:26:46 +0500

    update cal.js

--- cal.js
+++ cal.js
@@ -1,3 +1,23 @@
 
-let currentDisplay='';
-document.querySelector('#display').value=currentDisplay;
+let currentDisplay = '';
+const updateDisplay = () => {
+    document.querySelector('#display').value = currentDisplay;
+}
+const appendValue = (value) => {
+    currentDisplay += value;
+    updateDisplay();
+}
+const clearDisplay = () => {
+    currentDisplay = "";
+    updateDisplay();
+}
+const calculateResult = () => {
+    try {
+        currentDisplay = eval(currentDisplay).toString();
+    }
+    catch (e) {
+        currentDisplay = "Error";
+    }
+    updateDisplay();
+}
+updateDisplay();
